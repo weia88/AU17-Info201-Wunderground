fb9119d5e310671e515362afde31f0c5027e0a8e
Gol-Dann Slater <goldannrslater@gmail.com>
2017-12-04 18:09:11 -0800

    2 issues: capitalize first letter of city; fix hover break

--- analysis.R
+++ analysis.R
@@ -5,7 +5,7 @@ library(dplyr)
 # Note: In returned dataframe:
 # 'pop' = probability of precipitation.
 # 'qpf' = quantitative precipitation forecast
-GetForecast <- function(state, city){
+GetForecast <- function(state, city) {
   formatted.city <- gsub(" ", "_", city)
   url <- paste0("http://api.wunderground.com/api/", api.key, "/forecast10day/q/", state, "/", formatted.city, "/.json")
   request <- GET(url)
@@ -14,8 +14,9 @@ GetForecast <- function(state, city){
   return (simple.forecast)
 }
 
-GetValues <- function(location){
+GetValues <- function(location) {
   values <- location %>%
               select(date.day, date.month, date.year, high.fahrenheit, low.fahrenheit, pop, avehumidity)
   return(values)
 }
+
--- server.R
+++ server.R
@@ -10,31 +10,41 @@ shinyServer(function(input, output) {
     output$line.plot <- renderPlotly({
       
       # Filter data with state and city selection.
-      location1 <- GetForecast(input$initial.state, input$initial.city)
-      location1.data <- GetValues(location1)
-      location2 <- GetForecast(input$final.state, input$final.city)
-      location2.data <- GetValues(location2)
-      
+      location1.data <- GetValues(GetForecast(input$initial.state, input$initial.city))
+      location2.data <- GetValues(GetForecast(input$final.state, input$final.city))
+
       
       # Make Area Graph
       # 1st City Plot
       plot_ly(location1.data, x = ~date.day, y = ~high.fahrenheit, type = 'scatter', mode = 'lines',
-              line = list(color = 'blue'), name = paste(location1, 'High Temperatures')) %>%
+              line = list(color = 'blue'), name = paste(input$initial.city, 'Highs'), hoverinfo = 'text',
+              text = ~paste(input$initial.city,
+              '</br> High: ', high.fahrenheit)) %>%
       add_trace(y = ~low.fahrenheit, type = 'scatter', mode = 'lines', fill = 'tonexty',
                 fillcolor = 'rgba(0,100,80,0.2)', line = list(color = 'blue'),
-                name = paste(location1, 'Low Temperatures')) %>%
+                name = paste(input$initial.city, 'Lows'),  hoverinfo = 'text',
+                text = ~paste(input$initial.city,
+                '</br> Low: ', low.fahrenheit)) %>%
         
       # 2nd City Plot
       add_trace(x = ~date.day, y = ~location2.data$low.fahrenheit, type = 'scatter', mode = 'lines',
-                line = list(color = 'red'), name = paste(location2, 'High Temperatures')) %>%
+                line = list(color = 'red'), name = paste(input$final.city, 'Lows'),
+                hoverinfo = 'text',
+                text = ~paste(input$final.city,
+                '</br> Low: ', location2.data$low.fahrenheit)) %>%
       add_trace(y = ~location2.data$high.fahrenheit, type = 'scatter', mode = 'lines', fill = 'tonexty',
                 fillcolor = 'rgba(255, 0, 0, 0.2)', line = list(color = 'red'),
-                name = paste(location2, 'Low Temperatures')) %>%
+                name = paste(input$final.city, 'Highs'), 
+                hoverinfo = 'text',
+                text = ~paste(input$final.city,
+                '</br> High: ', location2.data$high.fahrenheit)) %>%
         
         
       layout(title = 'Low and High Temperature Comparison',
-             xaxis = list(title = 'Day'),
-             yaxis = list(title = 'Temperature (degrees F)'))
+             xaxis = list(title = 'Day', showgrid = TRUE, showline = FALSE, showticklabels = TRUE, dtick = 1),
+             yaxis = list(title = 'Temperature (degrees F)', showgrid = TRUE, showline = FALSE,
+              showticklabels = TRUE, tick.vals = 4),
+            autosize = FALSE, width = 900, height = 500)
       
   
     })
